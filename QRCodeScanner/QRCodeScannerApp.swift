7be4c73525c3c28f9c4c99eb08f6ae2b9a98b5d9
ParkYoungHo <enough6157@naver.com>
2024-05-13 14:48:36 +0900

    Initial Commit

---
+++ QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by 영호 박 on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
